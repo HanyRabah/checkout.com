dbcc2d7c6d137b46d9de4d2e73ecd19fa67f73c3
Hany Rabah <hanyrabah@Hanys-MacBook-Pro.local>
2020-06-21 19:51:45 +0400

    update Tooltip for deploy

--- components/ToolTip.js
+++ components/ToolTip.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import styled, { css } from 'styled-components'
 import { getTextWidth } from '../helpers/getTextWidth'
 
-const InfoIcon = () => (
-  <svg
+const InfoIcon = () => {
+  return (<svg
     fill="#fd496d"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
@@ -11,15 +11,15 @@ const InfoIcon = () => (
     height="16px"
   >
     <path d="M 7.5 1 C 3.917969 1 1 3.917969 1 7.5 C 1 11.082031 3.917969 14 7.5 14 C 11.082031 14 14 11.082031 14 7.5 C 14 3.917969 11.082031 1 7.5 1 Z M 7.5 2 C 10.542969 2 13 4.457031 13 7.5 C 13 10.542969 10.542969 13 7.5 13 C 4.457031 13 2 10.542969 2 7.5 C 2 4.457031 4.457031 2 7.5 2 Z M 7 4 L 7 5 L 8 5 L 8 4 Z M 7 6 L 7 11 L 8 11 L 8 6 Z" />
-  </svg>
-)
+  </svg>)
+}
 
 const Tooltip = (props) => {
   const [isShown, setIsShown] = useState(false)
   const { className, text } = props
 
   return (
-    <ToolTip className={className}>
+    <ToolTipWrapper className={className}>
       <div
         onMouseEnter={(e) => setIsShown(true)}
         onMouseLeave={() => setIsShown(false)}
@@ -29,11 +29,11 @@ const Tooltip = (props) => {
       <ToolTipInfo width={getTextWidth({ text })} isShown={isShown}>
         {text}
       </ToolTipInfo>
-    </ToolTip>
+    </ToolTipWrapper>
   )
 }
 
-const ToolTip = styled.div`
+const ToolTipWrapper = styled.div`
   display: inline-block;
   width: 16px;
   height: 16px;
